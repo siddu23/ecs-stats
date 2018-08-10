9fd3e7bf0fda8458b1ddb3ec1551fdb4b720b2ca
gauri <gauri@pratilipi.com>
2018-08-10 15:39:13 +0530

    [modify][author_dashboard] added route

--- router.py
+++ router.py
@@ -16,3 +16,6 @@ def app_routes(app):
     # /stats/v1.0/read_time?language=HINDI&category=romance&fromSec=0&toSec=120&limit=10&offset=0
     app.route('/stats/v1.0/read_time', ['GET', 'OPTIONS'], controller.get_read_time)
 
+    # /stats/v1.0/author_dashboard?authorId=2
+    app.route('/stats/v1.0/author_dashboard', ['GET', 'OPTIONS'], controller.get_author_dashboard)
+
