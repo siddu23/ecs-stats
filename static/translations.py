4582d960e8747287cbd54e6ac59421ffce30ac88
sachinpatil <sachinnitw1317@gmail.com>
2018-08-29 13:02:49 +0530

    added translations

---
+++ static/translations.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+translations = {
+  "bengali" : {
+    "publish" : "{author_name} প্রকাশিত করেছেন {story_name}",
+  },
+  "gujarati": {
+    "publish" : "{author_name} એ એક નવી રચના {story_name} પ્રકાશિત કરી છે.",
+  },
+  "hindi": {
+    "publish" : "{author_name} ने एक नयी रचना {story_name} प्रकाशित की है।",
+  },
+  "kannada": {
+    "publish" : "{author_name} ಅವರು {story_name} ಕೃತಿಯನ್ನು ಪ್ರಕಟಿಸಿದ್ದಾರೆ",
+  },
+  "malayalam": {
+    "publish" : "{author_name}, {story_name} എന്നൊരു രചന പ്രസിദ്ധീകരിച്ചിരിക്കുന്നു ",
+  },
+  "marathi": {
+    "publish" : "{author_name} ने नवीन साहित्य {story_name} प्रकाशित केले आहे ",
+  },
+  "tamil": {
+    "publish" : "{author_name} ஒரு படைப்பைப் பதிப்பித்துள்ளார் : {story_name}",
+  },
+  "telugu": {
+    "publish" : "{author_name} గారు {story_name} ప్రచురించారు",
+  },
+}
