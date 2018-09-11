adeb2601eceb7d640b633bb41a58d283c86e94ee
Suryadeep Pal <suryadeep10@gmail.com>
2018-09-11 13:08:10 +0530

    [DEBUG] Added debug logs

--- src/response_builder.py
+++ src/response_builder.py
@@ -301,6 +301,8 @@ def for_top_authors(kwargs):
     """top authors"""
     response_dict = {'authorList': []}
     authors = kwargs['authors']
+    print 'in response builder'
+    print authors
     logged_user_id = kwargs['logged_user_id']
     for author in authors:
         response = {}
