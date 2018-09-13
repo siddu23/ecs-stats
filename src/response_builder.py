7775ab3f65d5d62ffb38efd92baeb3cf25f3ee04
Suryadeep Pal <suryadeep10@gmail.com>
2018-09-13 21:08:51 +0530

    [FIX] Fix for rank

--- src/response_builder.py
+++ src/response_builder.py
@@ -320,7 +320,7 @@ def for_top_authors(kwargs):
         response = _set_key(response, 'followCount', data[author['author_id']]['followersCount'] if data != {} else 0)
         response_dict['authorList'].append(response)
 
-    if kwargs['rank']:
+    if kwargs['rank'] != None:
         response_dict['rank'] = kwargs['rank'] + 1 #because ranks are stored from 0 in redis
 
     return json.dumps(response_dict)
